87d88120926b0a8ab2d3d244f31550d8f51444e6
Eduardo A Fernandez Diaz <fernandez9000@gmail.com>
2018-12-22 13:55:57 -0600

    axios working now

---
+++ src/FetchDemo/FetchDemo.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class FetchDemo extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
+      .then(res => {
+        const posts = res.data.data.children.map(obj => obj.data);
+        this.setState({ posts });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{`/r/${this.props.subreddit}`}</h1>
+        <ul>
+          {this.state.posts.map(post =>
+            <li key={post.id}>{post.title}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default FetchDemo;
