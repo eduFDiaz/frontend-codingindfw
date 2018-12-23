e2fcabd9cde1c0980eac6e870333962c0bebf599
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2018-12-23 01:06:48 -0600

    Is working now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import './FetchDemo/FetchDemo'
+
 import FetchDemo from './FetchDemo/FetchDemo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FetchDemo subreddit="django"/>
+        {/* Dont include a slash to close thr domain ex: url=http://localhost:8000/ */}
+        <FetchDemo url="http://localhost:8000" path="/blog/api/posts/"/>
       </div>
     );
   }
--- src/FetchDemo/FetchDemo.js
+++ src/FetchDemo/FetchDemo.js
@@ -5,25 +5,42 @@ import axios from 'axios';
 
 class FetchDemo extends Component {
   state = {
-    posts: []
+    categories: []
   }
-
+  
   componentDidMount() {
-    axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
-      .then(res => {
-        const posts = res.data.data.children.map(obj => obj.data);
-        this.setState({ posts });
+    /* This component is invoked immediately after a component is mounted
+     (inserted into the tree). Initialization that requires DOM nodes 
+     should go here. If you need to load data from a remote endpoint, 
+     this is a good place to instantiate the network request.
+    */
+    console.log(`${this.props.url}${this.props.path}`);
+    axios.get(`${this.props.url}${this.props.path}`).then(res => {
+      console.log(res.data);
+      this.setState({
+        categories: res.data.results
       });
+      console.log('state cetegories: ',this.state.categories);
+    });
   }
-
+  
   render() {
+    const { categories } = this.state;
     return (
       <div>
-        <h1>{`/r/${this.props.subreddit}`}</h1>
+        <h1>{`${this.props.url}${this.props.path}`}</h1>
         <ul>
-          {this.state.posts.map(post =>
-            <li key={post.id}>{post.title}</li>
-          )}
+          {categories.length ? 
+            categories.map(cat => (
+              <li key={cat.id}>
+                <div>{cat.url}</div>
+                <div>{cat.title}</div>
+                <div>{cat.description}</div>
+                <div>{cat.text}</div>
+              </li>
+            ))
+            : 
+            (<li>Loading....</li>)}
         </ul>
       </div>
     );
