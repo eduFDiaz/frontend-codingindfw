9257d259e7592f36e3fab0c98e8c6b73fd0ae471
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-02-14 19:37:05 -0600

    post url changed to secure protocol

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -27,7 +27,7 @@ const Posts = (props) => {
                 actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
               >
                 <List.Item.Meta
-                  avatar={<Avatar size={64} src="http://www.codingindfw.com:8000/media/me.JPG" />}
+                  avatar={<Avatar size={64} src="https://www.codingindfw.com:8000/media/me.JPG" />}
                   title={<a href={`/posts/${item.id}`}>{item.title}</a>}
                   description={<Highlight innerHTML={true}>{item.description}</Highlight>}
                 />
--- src/containers/SiteLayout/SiteLayout.js
+++ src/containers/SiteLayout/SiteLayout.js
@@ -21,7 +21,7 @@ class SiteLayout extends Component {
     loading: true,
   }
   componentDidMount(){
-  var Url="http://www.codingindfw.com:8000/api/categories/";
+  var Url="https://www.codingindfw.com:8000/api/categories/";
   axios.get(`${Url}`)
       .then(response => {
         this.setState({
